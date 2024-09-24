74f9ed46fc12a7d5a19b162c0d0c5ee637d8d66e
Rizz <123063244+rizz-electrical-engg@users.noreply.github.com>
2024-09-24 15:23:52 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,9 +3,9 @@
 from decouple import config
 
 try:
-    APP_ID = config("21857983")
+    APP_ID = config("21857983, cast=int")
     API_HASH = config("e469e84c943ce3b8b056eb6a296f2c67")
-    BOT_TOKEN = config("7670762731:AAGiMUsnRysNwWFN_kKg7YQ61FXJ_pvLyjE")
+    BOT_TOKEN = config("")
     DEV = ""
     OWNER = config("833465134")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By   ⏤͟͞𝙍𝙞𝙯𝙯   (https://t.me/aboutRizzx)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
